16d75ff33f93c3a6c1fba40431bb13b6367fa46c
Soum1zz <soumallyanaskar2002@gmail.com>
2026-06-13 00:02:48 +0530

    v1.11

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -1,9 +1,11 @@
 import mercatoImg from '../assets/mercato-img.png'
 import chessifyImg from '../assets/chessify-img.png'
+import studyImg from '../assets/study-pic.png'
+
 import '../styles/projects.css'
 export default function Projects(){
     return(
-        <div className="sub-div">
+        <div className="proj-div">
             <div className='sub-title'>PROJECTS
             <div className='sub-hori-line'></div>
 
@@ -18,7 +20,7 @@ export default function Projects(){
                     style={{fontWeight:800, fontSize:"60px"}}
                     >Mercato</h2>
                     <div
-                    style={{letterSpacing:"5px"}}
+                    style={{letterSpacing:"3px"}}
                     >Fully functional e-commerce platform created using springboot framework.</div>
                 </div>
                 
@@ -32,8 +34,22 @@ export default function Projects(){
                     style={{fontWeight:800, fontSize:"60px"}}
                     >Chessify</h2>
                     <div
-                    style={{letterSpacing:"5px"}}
+                    style={{letterSpacing:"3px"}}
                     >Chess website created using react.</div>
+                </div>
+                
+            </div>
+            <div
+            className='proj-card'
+            >
+                <img src={studyImg} />
+                <div className='hover-text'>
+                    <h2
+                    style={{fontWeight:800, fontSize:"60px"}}
+                    >Study Helper Extension</h2>
+                    <div
+                    style={{letterSpacing:"3px"}}
+                    >Chrome extension for students to help them group videos by topics and keep notes </div>
                 </div>
                 
             </div>
